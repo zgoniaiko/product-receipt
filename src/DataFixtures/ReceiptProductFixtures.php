d3b3450d47438cf31ca22c7e9798cc76be8576de
Ivan Zgoniaiko <zgoniaiko@gmail.com>
2021-03-19 21:22:35 +0200

    Remove unused imports

--- src/DataFixtures/ReceiptProductFixtures.php
+++ src/DataFixtures/ReceiptProductFixtures.php
@@ -2,8 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Product;
-use App\Entity\Receipt;
 use App\Entity\ReceiptProduct;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
